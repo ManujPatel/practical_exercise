deca01118ed26966f08b11333d5a28b5709c2173
Manuj Patel <manuj.p@simformsolutions.com>
2022-05-25 18:00:50 +0530

    setup devise gem for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root'products#index'
+  devise_for :users
+  root "products#home"
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
